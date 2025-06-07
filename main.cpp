b984c3b55c480edad659af1ec7dcaf765b4c0c21
Psh <pyaesonehein81998@gmail.com>
2025-06-07 20:59:32 +0630

    [ADD]

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int width,height;
+
+  cout << "Type Width: ";
+  cin >> width;
+  cout << "Type Height: ";
+  cin >> height;
+
+  cout << "Area is " << width * height <<endl;
+  return 0;
+}
